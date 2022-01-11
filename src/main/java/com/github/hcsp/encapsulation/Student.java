6e39c97cc7e82cf5f29c4629790a19f03333e2c6
Leo Liu <leo.liu@vudoo.com>
2022-01-11 23:19:38 +1100

    vvvv

--- src/main/java/com/github/hcsp/encapsulation/Student.java
+++ src/main/java/com/github/hcsp/encapsulation/Student.java
@@ -10,4 +10,43 @@ public class Student {
 
     /** 分数 */
     private int score;
+
+    private boolean isFail;
+
+    public boolean isFail() {
+        return isFail;
+    }
+
+    public void setFail(boolean fail) {
+        if(this.score < 60) {
+            isFail = fail;
+        }
+        isFail = !fail;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isRetakingExam() {
+        return retakingExam;
+    }
+
+    public void setRetakingExam(boolean retakingExam) {
+        this.retakingExam = retakingExam;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+
 }
